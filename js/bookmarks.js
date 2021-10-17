21603048894dbfc653d3aa5165d83ab31c8131cf
zioan <zaharia.ioan@gmail.com>
2021-10-17 14:11:35 +0200

    more bugs fixed and style

--- js/bookmarks.js
+++ js/bookmarks.js
@@ -4,6 +4,8 @@ const urlSubmit = document.querySelector(".url-submit");
 const urlSearch = document.querySelector(".url-search");
 const output = document.querySelector(".bookmarks-output");
 const filter = document.querySelector(".filter");
+const removeAll = document.querySelector(".remove-all");
+const loadDemo = document.querySelector(".load-demo");
 
 function loadEvents() {
   document.addEventListener("DOMContentLoaded", getBookmarks);
@@ -13,11 +15,30 @@ function loadEvents() {
 
 loadEvents();
 
-// Debug, remove all
-// localStorage.removeItem("bookmarks");
+const defaultBookmarks = [
+  ["google", "https://www.google.com"],
+  ["stackoverflow", "https://www.stackoverflow.com"],
+  ["caniuse", "https://www.caniuse.com"],
+];
+
+loadDemo.addEventListener("click", () => {
+  if (localStorage.getItem("bookmarks") === null) {
+    bookmarks = defaultBookmarks;
+    localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
+    getBookmarks();
+  } else {
+    if (confirm("All Bookmarks will de deleted and demo content will load!")) {
+      bookmarks = defaultBookmarks;
+      localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
+      output.innerHTML = "";
+      getBookmarks();
+    }
+  }
+});
 
 function getBookmarks() {
   let bookmarks;
+
   if (localStorage.getItem("bookmarks") === null) {
     bookmarks = [];
   } else {
@@ -31,15 +52,21 @@ function getBookmarks() {
     const liUrl = document.createElement("a");
     const removeItem = document.createElement("a");
 
+    liTitle.classList.add("bookmark-title");
+    liLink.classList.add("bookmark-link");
+
     liTitle.appendChild(document.createTextNode(bookmark[0]));
-    liUrl.innerHTML = bookmark[1];
+    liUrl.innerHTML = bookmark[1].slice(12);
     liUrl.href = bookmark[1];
     liUrl.setAttribute("target", "_blank");
     liLink.appendChild(liUrl);
-    liUrl.classList.add("link-targer");
+    liUrl.classList.add("link-target");
 
     removeItem.className = "detele-item";
     removeItem.innerHTML = '<i class="far fa-trash-alt"></i>';
+    removeItem.addEventListener("click", function () {
+      liUrl.classList.toggle("remove-toggler");
+    });
     removeItem.addEventListener("click", removeBookmark);
     ul.appendChild(liTitle);
     ul.appendChild(liLink);
@@ -52,10 +79,16 @@ function getBookmarks() {
 function addBookmark(e) {
   if (urlTitle.value === "" && urlLink.value === "") {
     alert("Add a title and a link");
+    e.preventDefault();
+    return;
   } else if (urlTitle.value === "") {
     alert("Title is missing");
+    e.preventDefault();
+    return;
   } else if (urlLink.value === "") {
     alert("Add a link for your bookmark");
+    e.preventDefault();
+    return;
   }
 
   const ul = document.createElement("ul");
@@ -64,27 +97,35 @@ function addBookmark(e) {
   const liUrl = document.createElement("a");
   const removeItem = document.createElement("a");
 
+  liTitle.classList.add("bookmark-title");
+  liLink.classList.add("bookmark-link");
+
   const inputLink = urlLink.value.toLowerCase();
-  const linkPrexix = "https://www.";
+  const linkPrefix = "https://www.";
   let outputLink = "";
 
   if (inputLink.startsWith("www.")) {
-    outputLink = linkPrexix.concat(inputLink.slice(4));
+    outputLink = linkPrefix.concat(inputLink.slice(4));
+  } else if (inputLink.startsWith("https://")) {
+    outputLink = linkPrefix.concat(inputLink.slice(8));
   } else if (inputLink.startsWith("https://www.")) {
     outputLink = inputLink;
   } else {
-    outputLink = linkPrexix.concat(inputLink);
+    outputLink = linkPrefix.concat(inputLink);
   }
 
   liTitle.appendChild(document.createTextNode(urlTitle.value));
-  liUrl.innerHTML = outputLink;
+  liUrl.innerHTML = outputLink.slice(12);
   liUrl.href = outputLink;
   liUrl.setAttribute("target", "_blank");
   liLink.appendChild(liUrl);
-  liUrl.classList.add("link-targer");
+  liUrl.classList.add("link-target");
 
   removeItem.className = "detele-item";
   removeItem.innerHTML = '<i class="far fa-trash-alt"></i>';
+  removeItem.addEventListener("click", function () {
+    liUrl.classList.toggle("remove-toggler");
+  });
   removeItem.addEventListener("click", removeBookmark);
   ul.appendChild(liTitle);
   ul.appendChild(liLink);
@@ -102,6 +143,7 @@ function addBookmark(e) {
 
 function storeBookmark(title, url) {
   let bookmarks;
+
   if (localStorage.getItem("bookmarks") === null) {
     bookmarks = [];
   } else {
@@ -120,16 +162,15 @@ function removeBookmark(e) {
     bookmarks = JSON.parse(localStorage.getItem("bookmarks"));
   }
 
-  const target = document.querySelector(".link-targer");
+  const target = document.querySelector(".remove-toggler");
 
   bookmarks.forEach(function (bookmark, index) {
-    console.log(bookmark[1]);
-    console.log(target.innerHTML);
-    if (bookmark[1] === target.innerHTML) {
+    if (bookmark[1].slice(12) === target.innerHTML) {
       if (confirm(`Confirm to remove - ${bookmark[0]}`)) {
         e.target.parentElement.parentElement.remove();
 
         bookmarks.splice(index, 1);
+
         localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
       }
     }
@@ -146,9 +187,16 @@ function filterBookmarks(e) {
       itemTitle.toLowerCase().indexOf(text) != -1 ||
       itemLink.toLowerCase().indexOf(text) != -1
     ) {
-      bookmark.style.display = "block";
+      bookmark.style.display = "flex";
     } else {
       bookmark.style.display = "none";
     }
   });
 }
+
+removeAll.addEventListener("click", () => {
+  if (confirm("Are you sure you want to remove all bookmarks?")) {
+    localStorage.removeItem("bookmarks");
+    output.innerHTML = "";
+  }
+});
